ee0a741905e27137924eb234171e0eecba8ad875
Ken Chiem <59205333+kchiem12@users.noreply.github.com>
2021-10-30 13:31:53 -0400

    Delete python directory

--- python/mousestuff.py
+++
@@ -1,5 +0,0 @@
-from pynput.mouse import Buttom, Controller
-
-mouse = Controller()
-
-print("Current position is {}".format(mouse.position())
--- python/somethingstuff.py
+++
@@ -1,7 +0,0 @@
-
-def main():
-	#open the file. "r" means read
-	dictionary = open("engDictionary.txt", "r")
-	real_worlds = [line.strip() for line in dictionary.readLine()]
-
-main()
